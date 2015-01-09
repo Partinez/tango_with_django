edab9dd75f6ec0af29c6ef02e6ac09c4332e01e7
emMartin Basterrechea/em <Partinez@users.noreply.github.com>
2015-01-09 17:27:15 +0100

    Chapter 9 finished

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
 from django.forms.widgets import TextInput
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
 	name = forms.CharField(max_length=128, help_text="Please enter the category name.")
@@ -24,4 +25,14 @@ class PageForm(forms.ModelForm):
 		model = Page
 		exclude = ('category',)
 
+class UserForm(forms.ModelForm):
+	password = forms.CharField(widget = forms.PasswordInput())
 
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+	class Meta:
+		model = UserProfile
+		fields = ('website', 'picture')
