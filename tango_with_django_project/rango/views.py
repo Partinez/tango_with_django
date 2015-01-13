b1e9c2eba9cbe11e410aaa2231e63a04606e9957
emMartin Basterrechea/em <Partinez@users.noreply.github.com>
2015-01-13 01:14:58 +0100

    Completed chapter 15

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,4 @@
+from rango.bing_search import run_query
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
@@ -166,5 +167,17 @@ def restricted(request):
 
 
 
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
 
 
